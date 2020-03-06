6446029f3524af13c01b55b59781a0c071fe3550
Thomas Williams <tom.williams0593@outlook.com>
2020-03-06 13:40:53 +0000

    Add files via upload

--- MapTutorial/MapTutorial/ViewController.swift
+++ MapTutorial/MapTutorial/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         checkLocationServices()
-        //mapView.delegate=self
+        mapView.delegate=self
     }
     
     func checkLocationServices() {
