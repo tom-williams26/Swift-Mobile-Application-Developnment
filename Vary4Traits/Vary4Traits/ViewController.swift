c67f5fb98fdfd688ded0fff1bcfe504d7e6e9299
Thomas Williams <tom.williams0593@outlook.com>
2020-03-06 13:34:32 +0000

    Add files via upload

---
+++ Vary4Traits/Vary4Traits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vary4Traits
+//
+//  Created by user914215 on 3/6/20.
+//  Copyright © 2020 Tom Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Vary4Traits/Vary4TraitsTests/Vary4TraitsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Vary4TraitsTests.swift
+//  Vary4TraitsTests
+//
+//  Created by user914215 on 3/6/20.
+//  Copyright © 2020 Tom Williams. All rights reserved.
+//
+
+import XCTest
+@testable import Vary4Traits
+
+class Vary4TraitsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
